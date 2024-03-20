0803881300b36815f0bc5249cdf12747a71d983b
little颜 <dayanshifu@outlook.com>
2024-03-20 14:00:17 +0800

    6

--- 班级管理器.py
+++ 班级管理器.py
@@ -2,59 +2,80 @@ import os
 from sys import *
 import time
 import random as ran
+import win32con, win32api
 
-cmdmod = 0
+cmdmod = 0  #开启命令行模式选1
 title = '班级管理'
+FILEPATH = os.path.dirname(os.path.realpath(argv[0]))
+print(FILEPATH)
 
-try:
-    import easygui as box
-    from pypinyin import lazy_pinyin  
-except ImportError:
-    cmdmod = 1
-    op = input('必要的运行库未能导入，是否按“Y”现在开始安装 ')
-    if op == 'Y' or op == 'y':
-        os.system('pip install easygui')
-        os.system('pip install pypinyin')
-        print('运行库安装成功！请重新运行本程序')
-    print('必要的运行库未能导入，开启命令行模式，要使用完整的程序，请安装运行库')
-    sleep = 3
-    for i in range(3):
-        print('please wait for '+ str(sleep)+'s')
-        sleep -= 1
-        time.sleep(0.8)
+if cmdmod!=1:
+    try:
+        import easygui as box
+        from pypinyin import lazy_pinyin  
+    except ImportError:
+        op = input('必要的运行库未能导入，是否按“Y”现在开始安装 ')
+        if op == 'Y' or op == 'y':
+            os.system('pip install -i https://pypi.tuna.tsinghua.edu.cn/simple easygui')
+            os.system('pip install -i https://pypi.tuna.tsinghua.edu.cn/simple pypinyin')
+            os.system('pip install -i https://pypi.tuna.tsinghua.edu.cn/simple pypiwin32')
+            print('运行库安装成功！请重启程序！')
+            exit()
+        else:
+            print('必要的运行库未能导入，开启命令行模式，要使用完整的程序，请安装运行库')
+            cmdmod = 1
+            sleep = 3
+            for i in range(3):
+                print('please wait for '+ str(sleep)+'s')
+                sleep -= 1
+                time.sleep(0.8)
+
+def showf():win32api.SetFileAttributes(FILEPATH+'\\.classdata', win32con.FILE_ATTRIBUTE_NORMAL)
+def hidef():win32api.SetFileAttributes(FILEPATH+'\\.classdata', win32con.FILE_ATTRIBUTE_HIDDEN)
 
 if cmdmod != 1:
-    if 'database.txt' not in os.listdir():#检测数据库是否存在
-        op = box.ynbox('系统检测到你的文件目录没有 database.txt 文件，是否自动创建？', title=title)
-        if op == True:
-            f = open('database.txt', 'w')
-            res = f.write('{}')
-            f.close()
-            box.msgbox('创建成功！请重新运行本程序', title=title)
-            quit()
-        else:
-            box.msgbox('请自行在当前程序所在目录创建database.txt文件，输入一对英文{}并保存', title=title)
-            quit()
-    else:#检测数据库格式并读取
-        f = open('database.txt', 'r')
+    try:
+        showf()
+        f = open(FILEPATH+'\\.classdata', 'r')
         res = f.read()
         f.close()
+        hidef()
         if res == '':
-            f = open('database.txt', 'w')
+            showf()
+            f = open(FILEPATH+'\\.classdata', 'w')
             res = f.write('{}')
             f.close()
-            f = open('database.txt', 'r')
+            f = open(FILEPATH+'\\.classdata', 'r')
             res = eval(f.read())
             f.close()
+            hidef()
         else:
-            f = open('database.txt', 'r')
+            showf()
+            f = open(FILEPATH+'\\.classdata', 'r')
             res = eval(f.read())
             f.close()
+            hidef()
+    except:
+        op = box.ynbox('系统检测到你的文件目录没有 .classdata 文件，是否自动创建？', title=title)
+        if op == True:
+            try:showf()
+            except:pass
+            f = open(FILEPATH+'\\.classdata', 'w')
+            res = f.write('{}')
+            f.close()
+            hidef()
+            box.msgbox('创建成功！请重新运行本程序', title=title)
+            exit()
+        else:
+            box.msgbox('请自行在当前程序所在目录创建.classdata文件，输入一对英文{}并保存', title=title)
+            exit()
 
-    if 'admin' not in res:#设置admin密码并加密
+    if 'admin' not in res:
         while True:
-            ans = box.passwordbox('你还没有创建管理员密码，请输入将要使用的密码(8位以上)\n账号：admin', title=title)
-            if len(str(ans)) <= 8:
+            ans = box.passwordbox('你还没有创建管理员密码，请输入将要使用的密码(6位及以上)\n账号：admin', title=title)
+            if ans==None:
+                exit()
+            if len(str(ans)) < 6:
                 box.msgbox('密码不合法，请重新输入', title=title)
             else:
                 break
@@ -63,12 +84,14 @@ if cmdmod != 1:
             num = ord(w)
             code.append(num)
         res['admin'] = code
-        f = open('database.txt', 'w')
+        showf()
+        f = open(FILEPATH+'\\.classdata', 'w')
         res = f.write(str(res))
         f.close()
-        f = open('database.txt', 'r')
+        f = open(FILEPATH+'\\.classdata', 'r')
         res = eval(f.read())
         f.close()
+        hidef()
         code = res['admin']
         key = ''
         for w in code:
@@ -113,25 +136,29 @@ if cmdmod != 1:
                 continue
             res[op] = res[op] + int(mk)
             res['admin'] = admin
-            f = open('database.txt', 'w')
+            showf()
+            f = open(FILEPATH+'\\.classdata', 'w')
             res = f.write(str(res))
             f.close()
-            f = open('database.txt', 'r')
+            f = open(FILEPATH+'\\.classdata', 'r')
             res = eval(f.read())
             f.close()
+            hidef()
             res.pop('admin')
             box.msgbox('加分成功！\n'+ str(res), title=title)
         elif op == '管理':
-            f = open('database.txt', 'r')
+            showf()
+            f = open(FILEPATH+'\\.classdata', 'r')
             code = eval(f.read())['admin']
             f.close()
+            hidef()
             key = ''
             for w in code:
                 key = key + str(chr(w))
             ans = box.passwordbox('请输入管理员密码', title=title)
             if ans == key:
                 while True:
-                    op = box.buttonbox('选择你要做什么', choices=('添加', '删除', '清空', '重置', '排序', '退出'), title=title)
+                    op = box.buttonbox('选择你要做什么', choices=('添加', '删除', '清空', '排序', '退出'), title=title)
                     if op == '添加':
                         ans = box.enterbox('请输入学生姓名，输入中文“，”可添加多个', title=title)
                         if str(type(ans)) != "<class 'str'>":
@@ -143,12 +170,14 @@ if cmdmod != 1:
                                 res[i] = 0
                         else:
                             res[ans] = 0
-                        f = open('database.txt', 'w')
+                        showf()
+                        f = open(FILEPATH+'\\.classdata', 'w')
                         res = f.write(str(res))
                         f.close()
-                        f = open('database.txt', 'r')
+                        f = open(FILEPATH+'\\.classdata', 'r')
                         res = eval(f.read())
                         f.close()
+                        hidef()
                         res.pop('admin')
                         box.msgbox('创建成功！\n'+ str(res), title=title)
                     elif op == '删除':
@@ -165,12 +194,14 @@ if cmdmod != 1:
                                 continue
                             res.pop(ans)
                         res['admin'] = admin
-                        f = open('database.txt', 'w')
+                        showf()
+                        f = open(FILEPATH+'\\.classdata', 'w')
                         res = f.write(str(res))
                         f.close()
-                        f = open('database.txt', 'r')
+                        f = open(FILEPATH+'\\.classdata', 'r')
                         res = eval(f.read())
                         f.close()
+                        hidef()
                         res.pop('admin')
                         box.msgbox('创建成功！\n'+ str(res), title=title)
                     elif op == '清空':
@@ -178,14 +209,17 @@ if cmdmod != 1:
                         if op != True:
                             box.msgbox('取消', title=title)
                             continue
-                        res.clear()
+                        for i in res:
+                            res[i]=0
                         res['admin'] = admin
-                        f = open('database.txt', 'w')
+                        showf()
+                        f = open(FILEPATH+'\\.classdata', 'w')
                         res = f.write(str(res))
                         f.close()
-                        f = open('database.txt', 'r')
+                        f = open(FILEPATH+'\\.classdata', 'r')
                         res = eval(f.read())
                         f.close()
+                        hidef()
                         res.pop('admin')
                         box.msgbox('清空成功！\n'+ str(res), title=title)
                     elif op == '排序':
@@ -193,16 +227,18 @@ if cmdmod != 1:
                         sorted_data = {k: res[k] for k in sorted_keys}  
                         res=sorted_data
                         res['admin'] = admin
-                        f = open('database.txt', 'w')
+                        showf()
+                        f = open(FILEPATH+'\\.classdata', 'w')
                         res = f.write(str(res))
                         f.close()
-                        f = open('database.txt', 'r')
+                        f = open(FILEPATH+'\\.classdata', 'r')
                         res = eval(f.read())
                         f.close()
+                        hidef()
                         res.pop('admin')
                         box.msgbox('排序成功！\n'+ str(res), title=title)
                     elif op == '重置':
-                        box.msgbox('删除目录下 database.txt 文件即可（三思而后行）', title=title)
+                        box.msgbox('删除目录下 .classdata 文件即可（三思而后行）', title=title)
                     else:
                         break
             else:
@@ -222,9 +258,11 @@ if cmdmod != 1:
                 continue
             box.msgbox(ran.sample(lst, op), title=title)
         elif op == '排行榜':
-            f = open('database.txt', 'r')
+            showf()
+            f = open(FILEPATH+'\\.classdata', 'r')
             res = eval(f.read())
             f.close()
+            hidef()
             res.pop('admin')
             aaaa=sorted(res.items(),  key=lambda d:d[1], reverse=True)
             output_str=''
@@ -260,39 +298,39 @@ if cmdmod != 1:
         else:
             exit()
 else:
-    if 'database.txt' not in os.listdir():#检测数据库是否存在
-        op = input('系统检测到你的文件目录没有 database.txt 文件，是否按 Y 自动创建？')
+    if '.classdata' not in os.listdir():
+        op = input('系统检测到你的文件目录没有 .classdata 文件，是否按 Y 自动创建？')
         if op == 'Y' or op == 'y':
-            f = open('database.txt', 'w')
+            f = open(FILEPATH+'\\.classdata', 'w')
             res = f.write('{}')
             f.close()
             print('创建成功！请重新运行本程序')
             time.sleep(10)
-            quit()
+            exit()
         else:
-            print('请自行在当前程序所在目录创建database.txt文件，输入一对英文{}并保存')
+            print('请自行在当前程序所在目录创建.classdata文件，输入一对英文{}并保存')
             time.sleep(10)
-            quit()
-    else:#检测数据库格式并读取
-        f = open('database.txt', 'r')
+            exit()
+    else:
+        f = open(FILEPATH+'\\.classdata', 'r')
         res = f.read()
         f.close()
         if res == '':
-            f = open('database.txt', 'w')
+            f = open(FILEPATH+'\\.classdata', 'w')
             res = f.write('{}')
             f.close()
-            f = open('database.txt', 'r')
+            f = open(FILEPATH+'\\.classdata', 'r')
             res = eval(f.read())
             f.close()
         else:
-            f = open('database.txt', 'r')
+            f = open(FILEPATH+'\\.classdata', 'r')
             res = eval(f.read())
             f.close()
 
-    if 'admin' not in res:#设置admin密码并加密
+    if 'admin' not in res:
         while True:
-            ans = input('你还没有创建管理员密码，请输入将要使用的密码(8位以上)\n账号：admin')
-            if len(str(ans)) <= 8:
+            ans = input('你还没有创建管理员密码，请输入将要使用的密码(6位及以上)\n账号：admin')
+            if len(str(ans)) < 6:
                 print('密码不合法，请重新输入', title=title)
             else:
                 break
@@ -301,10 +339,10 @@ else:
             num = ord(w)
             code.append(num)
         res['admin'] = code
-        f = open('database.txt', 'w')
+        f = open(FILEPATH+'\\.classdata', 'w')
         res = f.write(str(res))
         f.close()
-        f = open('database.txt', 'r')
+        f = open(FILEPATH+'\\.classdata', 'r')
         res = eval(f.read())
         f.close()
         code = res['admin']
@@ -336,16 +374,16 @@ else:
                 continue
             res[op] = res[op] + int(mk)
             res['admin'] = admin
-            f = open('database.txt', 'w')
+            f = open(FILEPATH+'\\.classdata', 'w')
             res = f.write(str(res))
             f.close()
-            f = open('database.txt', 'r')
+            f = open(FILEPATH+'\\.classdata', 'r')
             res = eval(f.read())
             f.close()
             res.pop('admin')
             print('加分成功！\n'+ str(res))
         elif op == '3':
-            f = open('database.txt', 'r')
+            f = open(FILEPATH+'\\.classdata', 'r')
             code = eval(f.read())['admin']
             f.close()
             key = ''
@@ -354,7 +392,7 @@ else:
             ans = input('请输入管理员密码')
             if ans == key:
                 while True:
-                    op = input('选择你要做什么\n,添加 0, 删除 1, 清空 2, 重置 3, 退出 4')
+                    op = input('选择你要做什么\n,添加 0, 删除 1, 清空 2, 退出 3')
                     if op == '0':
                         ans = input('请输入学生姓名，输入中文“，”可添加多个')
                         if str(type(ans)) != "<class 'str'>":
@@ -366,10 +404,10 @@ else:
                                 res[i] = 0
                         else:
                             res[ans] = 0
-                        f = open('database.txt', 'w')
+                        f = open(FILEPATH+'\\.classdata', 'w')
                         res = f.write(str(res))
                         f.close()
-                        f = open('database.txt', 'r')
+                        f = open(FILEPATH+'\\.classdata', 'r')
                         res = eval(f.read())
                         f.close()
                         res.pop('admin')
@@ -388,10 +426,10 @@ else:
                                 continue
                             res.pop(ans)
                         res['admin'] = admin
-                        f = open('database.txt', 'w')
+                        f = open(FILEPATH+'\\.classdata', 'w')
                         res = f.write(str(res))
                         f.close()
-                        f = open('database.txt', 'r')
+                        f = open(FILEPATH+'\\.classdata', 'r')
                         res = eval(f.read())
                         f.close()
                         res.pop('admin')
@@ -401,19 +439,18 @@ else:
                         if op != '1':
                             print('取消')
                             continue
-                        res.clear()
+                        for i in res:
+                            res[i]=0
                         res['admin'] = admin
-                        f = open('database.txt', 'w')
+                        f = open(FILEPATH+'\\.classdata', 'w')
                         res = f.write(str(res))
                         f.close()
-                        f = open('database.txt', 'r')
+                        f = open(FILEPATH+'\\.classdata', 'r')
                         res = eval(f.read())
                         f.close()
                         res.pop('admin')
                         print('清空成功！\n'+ str(res))
                     elif op == '3':
-                        print('删除目录下 database.txt 文件即可（三思而后行）')
-                    elif op == '4':
                         break
                     else:
                         pass
@@ -434,11 +471,40 @@ else:
                 continue
             print(ran.sample(lst, op))
         elif op == '2':
-            f = open('database.txt', 'r')
+            f = open(FILEPATH+'\\.classdata', 'r')
             res = eval(f.read())
             f.close()
             res.pop('admin')
-            print(sorted(res.items(),  key=lambda d:d[1], reverse=True))
+            
+            aaaa=sorted(res.items(),  key=lambda d:d[1], reverse=True)
+            output_str=''
+            for index, (key, value) in enumerate(aaaa, start=1):  
+                output_str += f"{index}: {key} {value}||￷|" 
+
+            lstt = list(output_str.split('||￷|'))
+            
+            lst = []
+            aaa = 0
+            bbb = 1
+            ccc = {}
+            for l in lstt:
+                aaa+=1
+                lst.append(l)
+                if aaa >=10:
+                    aaa=0
+                    ccc[bbb]=lst
+                    bbb+=1
+                    lst = []
+                    
+            aaa=0
+            ccc[bbb]=lst
+            bbb+=1
+            lst = []
+            straaa=''
+            for i in ccc:
+                for j in range(0,len(ccc[i])):
+                    straaa+=ccc[i][j]+'\n'
+            print(straaa)
             pass
         elif op == '4':
             exit()
